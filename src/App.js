bf4eef745fd2b65183843b85d5235835ca8e39b8
Beatrice T <be@trice.codes>
2020-12-24 20:07:13 -0800

    TimelineEvent renders hardcoded data (wave 1)'

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent'
 
 function App() {
+  let event={ 
+    person: 'Adele Goldberg', 
+    status: 'In Smalltalk, everything happens somewhere else.', 
+    timeStamp: '2018-05-18T22:12:03Z' 
+  } 
   console.log(timelineData);
 
   // Customize the code below
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">{timelineData.person}'s Social Media feed</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent event={event}/>
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
+import PropTypes from 'prop-types';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (event) => {
+  console.log(event.event);
+  console.log(event.person);
+  return (
+    <section className='timeline-event'>
+      <div className='event-person'>
+        {event.event.person}
+      </div>
+      <div className='event-status'>
+        {event.event.status}
+      </div>
+      <time className='event-time'>
+        <Timestamp time={event.event.timeStamp} />
+      </time>
+    </section>
+  );
+};
+
+TimelineEvent.propTypes = {
+  event: PropTypes.shape({
+    person: PropTypes.string,
+    status: PropTypes.string,
+    timeStamp: PropTypes.string
+  })
 }
 
 export default TimelineEvent;
